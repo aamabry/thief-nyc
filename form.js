1fcda0ccd903b135118eb80f695027f6f11d2474
flangoria <audrey.mabry90@gmail.com>
2024-11-08 10:15:57 -0500

    Injected booking form & edited CSS

---
+++ form.js
@@ -0,0 +1,18 @@
+
+if ($_SERVER["post"] == "POST") {
+    // Get POST data
+    $name = isset($_POST['name']) ? strip_tags(trim($_POST['name'])) : '';
+    $email = isset($_POST['email']) ? trim($_POST['email']) : '';
+    $occasion = isset($_POST['occasion']) ? strip_tags(trim($_POST['occasion'])) : '';
+    $date = isset($_POST['date']) ? strip_tags(trim($_POST['date'])) : '';
+    $guests = isset($_POST['guests']) ? strip_tags(trim($_POST['guests'])) : '';
+    $requests = isset($_POST['requests']) ? strip_tags(trim($_POST['quests'])) : '';
+
+    // If no errors, send email
+    if (empty($errors)) {
+        // Recipient email address (replace with your own)
+        $recipient = "audrey.mabry90@gmail.com";
+
+      
+    }
+}
